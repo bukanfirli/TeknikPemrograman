d064ddddef66ebc4e27bdaee856de2a70ae7598e
Firliana Febrina <firliana.febrina.tif425@polban.ac.id>
2026-04-01 21:13:30 +0700

    Update ProblemOne (revert to correct output)

--- W6/JavaProblems/ProblemOne.java
+++ W6/JavaProblems/ProblemOne.java
@@ -7,10 +7,10 @@ import java.util.stream.Collectors;
 public class ProblemOne {
     public static void main(String[] args) {
         ArrayList<Employee> list = new ArrayList<>();
-        list.add(new Employee("John", 10000));
-        list.add(new Employee("Rob", 40000));
-        list.add(new Employee("Bob", 70000));
-        list.add(new Employee("Alice", 50000));
+        list.add(new Employee("John", 50000));
+        list.add(new Employee("Rob", 70000));
+        list.add(new Employee("Bob", 40000));
+        list.add(new Employee("Alice", 10000));
 
         List<Employee> sortedEmp = list.stream()
                 .sorted((e1, e2) -> e1.getName().compareTo(e2.getName()))
