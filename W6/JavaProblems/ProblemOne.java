d9380885defdcacb24eaaabcc9ad2ab8fd39209e
Firliana Febrina <firliana.febrina.tif425@polban.ac.id>
2026-04-01 14:15:36 +0700

    Add sorting and stream operations

---
+++ W6/JavaProblems/ProblemOne.java
@@ -0,0 +1,23 @@
+package JavaProblems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProblemOne {
+    public static void main(String[] args) {
+        ArrayList<Employee> list = new ArrayList<>();
+        list.add(new Employee("John", 10000));
+        list.add(new Employee("Rob", 40000));
+        list.add(new Employee("Bob", 70000));
+        list.add(new Employee("Alice", 50000));
+
+        List<Employee> sortedEmp = list.stream()
+                .sorted((e1, e2) -> e1.getName().compareTo(e2.getName()))
+                .collect(Collectors.toList());
+
+        for (Employee e : sortedEmp) {
+            System.out.println(e);
+        }
+    }
+}
